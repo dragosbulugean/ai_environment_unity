2a0522ad3473bbd92091af73bb3712f77257c8ea
Dragos Bulugean <dragos.bulugean@gmail.com>
2015-10-14 16:14:31 +0300

    Add objects to study

---
+++ Assets/RotationController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationController : MonoBehaviour {
+
+    public float RotationSpeed = 10.0f;
+    public bool Clockwise = false;
+  
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float amount = Time.deltaTime * RotationSpeed;
+        amount = Clockwise ? amount : -amount;
+        this.transform.Rotate(0, Time.deltaTime * RotationSpeed, 0, Space.World);
+	}
+}
